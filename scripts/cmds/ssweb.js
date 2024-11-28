31621f3894a2135a7b1438b04b95f99aecf465c4
Popkid <popkidtechs@gmail.com>
2024-11-29 00:50:59 +0300

    ⭕POP KID⭕

--- scripts/cmds/ssweb.js
+++ scripts/cmds/ssweb.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 module.exports = {
     config: {
         name: "screenshot",
-        author: "keithkeizzah",
+        author: "popkid",
         description: "Take a screenshot of a webpage or Google search results",
         category: "tools",
         usage: "/screenshot <url>\n or\n/screenshot -g <text>",
